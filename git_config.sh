38828c2d6dc00c6d7051d3b3fed901e309285dce
Samuel Leong <leowhiz@yahoo.com.sg>
2020-05-27 12:19:56 +0800

    updated git config

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-git config --global alias.update '!git pull && git submodule update --init --recursive'
+git config --global alias.subinit '!git pull && git submodule update --init --recursive'
+git config --global alias.update '!git pull && git submodule update --remote --recursive --merge'
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
 git config --globa alias.stat 'status'
