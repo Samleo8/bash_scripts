2e67410c880218133528cd020c8f06017f227180
Samuel Leong <leowhiz@yahoo.com.sg>
2022-09-26 14:25:18 -0400

    update ssh keygen script

--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -2,13 +2,13 @@
 sudo apt install keychain
 ssh-keygen -t ed25519 -C samleocw@gmail.com
 
-# Print the public key to copy to GitHub
-echo "Copy the following public key to GitHub > Settings > Add SSH key"
-cat ~/.ssh/id_ed25519.pub
+# Add the following to ~/.bashrc
+echo "eval `keychain --eval --agents ssh id_ed25519`" >> `~/.bashrc`
 
 # Start the ssh-agent in the background
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
-# Add to ssh config
-echo -e "Host *\n    AddKeysToAgent yes\n    UseKeychain yes\n    IdentityFile ~/.ssh/id_ed25519" >>~/.ssh/config
+# Print the public key to copy to GitHub
+echo "Copy the following public key to GitHub > Settings > Add SSH key"
+cat ~/.ssh/id_ed25519.pub
